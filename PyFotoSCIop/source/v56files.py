3cf7ad7127cde6f28cfe05733f209d7355900ddf
Caleb Hulbert <cmhulbert@gmail.com>
2018-02-03 15:25:00 -0500

    change to relative path

--- PyFotoSCIop/source/v56files.py
+++ PyFotoSCIop/source/v56files.py
@@ -1,7 +1,7 @@
 import os
 import struct
 import sys
-sys.path.append('C:\\Users\\caleb\\PycharmProjects\\World_Editor')
+sys.path.append('..\\..\\')
 
 from PyFotoSCIop.source.palette import Palette
 from PyFotoSCIop.source.scicell import ViewCellHeader, Cell
